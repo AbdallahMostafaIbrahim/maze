9955b6146b374ab33ef9362b4803202fb38752a7
AbdallahMostafaIbrahim <abdallahmostafaibrahim@gmail.com>
2024-05-05 16:18:04 +0300

    map init

--- mazescene.cpp
+++ mazescene.cpp
@@ -4,6 +4,7 @@
 #include <QFile>
 #include <QDebug>
 #include <QMessageBox>
+#include <QGraphicsPixmapItem>
 
 MazeScene::MazeScene() {
     QFile file(":/map/file.txt");
@@ -11,20 +12,25 @@ MazeScene::MazeScene() {
         QMessageBox::critical(0, "Error", "Couldn't load level");
     }
     QTextStream stream(&file);
-    int block_size = 24;
+    int block_size = 33;
     int value;
     setSceneRect(0, 0, 800,800);
 
-    for(int i = 0; i < 33; i++) {
-        for(int j = 0; j < 33; j++) {
+    for(int i = 0; i < 24; i++) {
+        for(int j = 0; j < 24; j++) {
             stream >> value;
             if(value == 0) {
                 addRect(j * block_size, i * block_size, block_size, block_size, QPen(Qt::black), QBrush(Qt::black));
             }
             if(value == 1) {
-                addRect(j * block_size, i * block_size, block_size, block_size, QPen(Qt::black), QBrush(Qt::yellow));
+                addRect(j * block_size, i * block_size, block_size, block_size, QPen(Qt::black), QBrush(Qt::blue));
             }
         }
     }
+
+    QGraphicsPixmapItem* flag = new QGraphicsPixmapItem(QPixmap((":/images/flag.png")).scaledToWidth(block_size));
+    flag->setPos(22 * block_size, 22 * block_size);
+    addItem(flag);
+
     file.close();
 }
